7921d7eae3653399192e33e0548f0e72814ada1a
EdsonGomesJR <edyz86@hotmail.com>
2020-01-06 14:30:33 -0300

    - Montagem do Layoutd de Login - Configuração do Calligraph no MainActivity

--- app/src/main/java/com/edson/uberclone/MainActivity.java
+++ app/src/main/java/com/edson/uberclone/MainActivity.java
@@ -2,13 +2,31 @@ package com.edson.uberclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    //adiciona o Calligraphy  ao contexto base
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //config o calligraphy para alterar a font do texto *OBS tem que estar antes do setContentView
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/Arkhip_font.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build());
+
         setContentView(R.layout.activity_main);
     }
 }
