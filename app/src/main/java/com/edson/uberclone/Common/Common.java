54cf99cf22e571d7fd71f6adb27b54535cf16fae
EdsonGomesJR <edyz86@hotmail.com>
2020-01-09 10:45:41 -0300

    Configurada a classe Common

--- app/src/main/java/com/edson/uberclone/Common/Common.java
+++ app/src/main/java/com/edson/uberclone/Common/Common.java
@@ -1,4 +1,14 @@
 package com.edson.uberclone.Common;
 
+import com.edson.uberclone.Remote.IGoogleAPI;
+import com.edson.uberclone.Remote.RetrofitClient;
+
 public class Common {
+
+    public static final String baseURL = "https://maps.googleapis.com";
+
+    public static IGoogleAPI getGoogleAPI() {
+
+        return RetrofitClient.getClient(baseURL).create(IGoogleAPI.class);
+    }
 }
