a198ca3672f1dc9bd0377355b119084085e5f8cd
EdsonGomesJR <edyz86@hotmail.com>
2020-01-21 09:35:01 -0300

    Alterações na classe Common para utilizar o FCM

--- app/src/main/java/com/edson/uberclone/Common/Common.java
+++ app/src/main/java/com/edson/uberclone/Common/Common.java
@@ -1,20 +1,33 @@
 package com.edson.uberclone.Common;
 
+import android.location.Location;
+
+import com.edson.uberclone.Remote.FCMClient;
+import com.edson.uberclone.Remote.IFCMService;
 import com.edson.uberclone.Remote.IGoogleAPI;
 import com.edson.uberclone.Remote.RetrofitClient;
 
 public class Common {
 
+
     public static final String driver_tbl = "Drivers";
     public static final String user_driver_tbl = "DriversInformation";
     public static final String user_rider_tbl = "RiderInformation";
     public static final String pickup_request_tbl = "PickupRequest";
+    public static final String token_tbl = "Tokens";
+    public static final String fcmURL = "https://fcm.googleapis.com";
 
 
     public static final String baseURL = "https://maps.googleapis.com";
+    public static Location mLastLocation = null;
 
     public static IGoogleAPI getGoogleAPI() {
 
         return RetrofitClient.getClient(baseURL).create(IGoogleAPI.class);
+    }
+
+    public static IFCMService getFCMService() {
+
+        return FCMClient.getClient(fcmURL).create(IFCMService.class);
     }
 }
