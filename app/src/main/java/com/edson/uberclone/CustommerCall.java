9b30aa683fe76dd82540a5bea3743ba425d0e209
EdsonGomesJR <edyz86@hotmail.com>
2020-01-21 09:55:53 -0300

    Classe que retorna os valores do json da requisição feita pelo rider, e exibe como notificação ao driver

---
+++ app/src/main/java/com/edson/uberclone/CustommerCall.java
@@ -0,0 +1,152 @@
+package com.edson.uberclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.animation.LinearInterpolator;
+import android.widget.TextView;
+
+import com.edson.uberclone.Common.Common;
+import com.edson.uberclone.Remote.IFCMService;
+import com.edson.uberclone.Remote.IGoogleAPI;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.JointType;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.SquareCap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class CustommerCall extends AppCompatActivity {
+
+    TextView txtTime, txtAddress, txtDistance;
+    MediaPlayer mediaPlayer;
+    IGoogleAPI mService;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custommer_call);
+
+        mService = Common.getGoogleAPI();
+
+        //init view
+        txtTime = findViewById(R.id.txtTime);
+        txtAddress = findViewById(R.id.txtAddress);
+        txtDistance = findViewById(R.id.txtDistance);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.ringtone);
+
+        if (getIntent() != null) {
+
+            double lat = getIntent().getDoubleExtra("lat", -1.0);
+            double lng = getIntent().getDoubleExtra("lng", -1.0);
+
+            //just copy getDirection from Welcome
+            getDirection(lat, lng);
+        }
+
+    }
+
+    private void getDirection(double lat, double lng) {
+
+
+        String requestApi = null;
+
+        try {
+
+            requestApi = "https://maps.googleapis.com/maps/api/directions/json?" +
+                    "mode=driving&" +
+                    "transit_routing_preference=less_driving&" +
+                    "origin=" + Common.mLastLocation.getLatitude() + "," + Common.mLastLocation.getLongitude() + "&" +
+                    "destination=" + lat + " , " + lng + "&" +
+                    "key=" + getResources().getString(R.string.google_direction_api);
+            Log.d("Eds", requestApi);//print url for debug
+
+            mService.getPath(requestApi)
+                    .enqueue(new Callback<String>() {
+                        @Override
+                        public void onResponse(Call<String> call, Response<String> response) {
+
+                            try {
+                                JSONObject jsonObject = new JSONObject(response.body());
+
+                                JSONArray routes = jsonObject.getJSONArray("routes");
+
+                                //after get routes, just get first element of routes
+                                JSONObject object = routes.getJSONObject(0);
+
+                                //after get first element, we need to get array with name "legs"
+                                JSONArray legs = object.getJSONArray("legs");
+
+                                //get first element of legs array
+                                JSONObject legsObject = legs.getJSONObject(0);
+
+                                //Now, getDistance
+                                JSONObject distance = legsObject.getJSONObject("distance");
+                                txtDistance.setText(distance.getString("text"));
+
+                                //get Time
+                                JSONObject time = legsObject.getJSONObject("duration");
+                                txtTime.setText(time.getString("text"));
+
+                                //get address
+                                String address = legsObject.getString("end_address");
+                                txtAddress.setText(address);
+
+
+                            } catch (JSONException e) {
+
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<String> call, Throwable t) {
+
+                        }
+                    });
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void onStop() {
+        mediaPlayer.release();
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        mediaPlayer.release();
+        super.onPause();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        mediaPlayer.start();
+    }
+}
